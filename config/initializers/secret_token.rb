71aca7e43b3d35de78753efcaa3d2796f777e62f
cosaki <cosaki@vip.qq.com>
2013-09-12 13:06:19 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Leo::Application.config.secret_key_base = '1dd8747ada14b02583cf5e0fdf1f397e9a55414a253252ef2b0aabae59af631023799709ba808b07ea904b2da640addf9c4da55ed8f07c16a7eb77b443a27445'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Leo::Application.config.session_store :cookie_store, key: '_leo_session'
